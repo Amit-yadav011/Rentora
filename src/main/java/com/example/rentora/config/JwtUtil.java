a225b3bbc88f27af272ae883bf46444e2e0fc492
Amit <amit457yadav@gmail.com>
2026-04-22 22:20:31 +0530

    fix jwt secret usage

--- src/main/java/com/example/rentora/config/JwtUtil.java
+++ src/main/java/com/example/rentora/config/JwtUtil.java
@@ -2,11 +2,14 @@ package com.example.rentora.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 import java.security.Key;
 import java.util.Date;
+
+
 
 @Component
 public class JwtUtil {
@@ -14,19 +17,21 @@ public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;// must be long of 32 or bits
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(key)
+                .signWith(getKey())
                 .compact();
     }
 
     public String extractEmail(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(getKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
